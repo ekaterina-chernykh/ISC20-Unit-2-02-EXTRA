5edbb97a597a631127989492b4f3c40ef7ac8197
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-09 04:13:51 +0000

    done

---
+++ main.cs
@@ -0,0 +1,17 @@
+// Created by: Ekaterina
+// Created on: Oct 2022
+//
+// This program show area and perimeter of a rectangle
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("The dimensions of a rectangle are: 5 cm and 3 cm.");
+      Console.WriteLine("The area is: " + (5 * 3) + "cm^2");
+      Console.WriteLine("The perimter is: " + ((5 + 3) * 2) + "cm ");
+        Console.WriteLine("\nDone.");
+    }
+}
