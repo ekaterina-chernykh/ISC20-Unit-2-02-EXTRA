0d4b2805a58309e77a6519fd604e1ba5ce863a73
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-10 19:21:11 +0000

    temp version of the project  with printing "Hi" to fix whitespace formatting compilation error

--- main.cs
+++ main.cs
@@ -7,11 +7,8 @@ using System;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-       Console.WriteLine("The dimensions of a rectangle are: 5 cm and 3 cm.");
-       Console.WriteLine("The area is: " + (5 * 3) + "cm^2");
-       Console.WriteLine("The perimter is: " + ((5 + 3) * 2) + "cm ");
-       Console.WriteLine("\nDone.");
-    }
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Hi");
+  }
 }
