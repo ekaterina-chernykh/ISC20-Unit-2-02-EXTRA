ca94ab1d99c4daaf059d30ce5f06ad16a5b04f8e
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-10 19:28:33 +0000

    copy example from Mr. Coxall

--- main.cs
+++ main.cs
@@ -3,12 +3,19 @@
 //
 // This program show area and perimeter of a rectangle
 
-using System;
+using System; 
 
-public class Program
+public class Program 
 {
-  public static void Main(string[] args)
-  {
-    Console.WriteLine("Hi");
-  }
-}
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine(9 + 2);
+        Console.WriteLine("7 - 3 = " + (7 - 3));
+        Console.WriteLine("4 * 2 = " + (4 * 2));
+        Console.WriteLine("4 + 4 ÷ 2 = " + (4 + (4 / 2)));
+        Console.WriteLine("5 + 2³ = " + (5 + Math.Pow(2, 3)));
+
+        Console.WriteLine("\nDone.");
+    }
+}  
