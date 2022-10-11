0180fc8d539220a8bf5380fa57d713e5ee04906b
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-11 17:59:09 +0000

    whiteline fix?

--- main.cs
+++ main.cs
@@ -3,16 +3,15 @@
 //
 // This program show area and perimeter of a rectangle
 
-using System; 
+using System;
 
-public class Program 
+public class Program
 {
-   public static void Main(string[] args)
-   {
-       Console.WriteLine("The dimensions of a rectangle are: 5 cm and 3 cm.");
-       Console.WriteLine("The area is: " + (5 * 3) + "cm^2");
-       Console.WriteLine("The perimter is: " + ((5 + 3) * 2) + "cm ");
-       Console.WriteLine("\nDone.");
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("The dimensions of a rectangle are: 5 cm and 3 cm.");
+        Console.WriteLine("The area is: " + (5 * 3) + "cm²");
+        Console.WriteLine("The perimter is: " + ((5 + 3) * 2) + "cm ");
+        Console.WriteLine("\nDone.");
     }
 }
-  
